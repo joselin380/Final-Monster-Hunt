f25eb44cdaf3c5e01ee94547d8ba42f1b3691035
Joselin <142624013+joselin380@users.noreply.github.com>
2023-11-30 12:02:01 -0500

    Add box colliders
    Added a box collider to each enemy prefab, player, and projectile bullets. Also added triggers and removed gravity on bullet

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -12,7 +12,7 @@ public class SpawnManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        // Randomly spawn enemies
+        // Call method to randomly spawn enemies
         InvokeRepeating("SpawnRandomEnemies", startDelay, spawnInterval);
     }
 
@@ -25,7 +25,7 @@ public class SpawnManager : MonoBehaviour
     void SpawnRandomEnemies()
     {
         int enemyIndex = Random.Range(0, enemiesPrefab.Length);
-        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnRangeZ);
-        Instantiate(enemiesPrefab[enemyIndex], new Vector3(0, 0, 20), enemiesPrefab[enemyIndex].transform.rotation);
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, Random.Range(-spawnRangeZ, spawnRangeZ));
+        Instantiate(enemiesPrefab[enemyIndex], spawnPos, enemiesPrefab[enemyIndex].transform.rotation);
     }
 }
