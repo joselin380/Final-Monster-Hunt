740c4fbb5a267c20600e609e703f40fdc21d3af4
Joselin <142624013+joselin380@users.noreply.github.com>
2023-12-01 03:50:49 -0500

    Rotate Player
    Added to player script so it can rotate to direction where mouse is at

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,11 +10,12 @@ public class PlayerController : MonoBehaviour
     public float speed = 10.0f;
     private float xRange = 30;
     private float zRange = 8;
+    private Camera mainCamera;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        mainCamera = FindObjectOfType<Camera>();
     }
 
     // Update is called once per frame
@@ -43,10 +44,22 @@ public class PlayerController : MonoBehaviour
             transform.position = new Vector3(transform.position.x, transform.position.y, zRange);
         }
 
-        // launch bullets
-        if (Input.GetKeyDown(KeyCode.Space))
+        // Launch bullets
+        if (Input.GetMouseButtonDown(0))
         {
-            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+            Instantiate(projectilePrefab, transform.position, transform.rotation);
         }
+
+        // Face Mouse
+        Ray cameraRay = mainCamera.ScreenPointToRay(Input.mousePosition);
+        Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
+        float rayLength;
+        if (groundPlane.Raycast(cameraRay, out rayLength))
+        {
+            Vector3 pointToLook = cameraRay.GetPoint(rayLength);
+            Debug.DrawLine(cameraRay.origin, pointToLook, Color.yellow); // helps in scene view to see where he is looking
+            transform.LookAt(new Vector3(pointToLook.x, transform.position.y, pointToLook.z));
+        }
+        
     }
 }
